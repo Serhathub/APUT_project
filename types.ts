1f7dc6393f20f8c91d850492f1699c2f0d718f63
Serhat Kaya <s133026@ap.be>
2025-05-22 13:16:39 +0000

    clubDetail changed + code refactored

--- types.ts
+++ types.ts
@@ -18,9 +18,25 @@ export interface User {
   blacklistedClubs: BlacklistedClub[];
 }
 
+export interface Coach {
+  name: string;
+}
+export interface Area {
+  id: number;
+  name: string;
+  code: string;
+  flag: string;
+}
+export interface Player {
+  id: number;
+  name: string;
+  position: string;
+  nationality: string;
+}
 export interface Club {
   _id?: ObjectId;
   id: number;
+  area?: Area;
   name: string;
   shortName: string;
   tla: string;
@@ -31,6 +47,10 @@ export interface Club {
   clubColors: string;
   venue: string;
   lastUpdated: string;
+  coach?: Coach;
+  squad?: Player[];
+  staff?: any[];
+  league?: string;
 }
 export interface League {
   _id?: ObjectId;
